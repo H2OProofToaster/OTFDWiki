32af0c2b118ebb89ed9d02f6d991999b5f87d3b5
Nick <188719678+H2OProofToaster@users.noreply.github.com>
2025-12-01 11:57:48 -0800

    Why is this a thing

--- src/app/components/infoBox.tsx
+++ src/app/components/infoBox.tsx
@@ -1,1 +1,9 @@
-export default function infoBox
+export default function InfoBox() {
+    return (
+        <div class="infobox-wrapper">
+            <div class="title">
+                <p>
+                    TITLE
+                </p>
+            </div>
+        </div>
